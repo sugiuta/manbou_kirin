390d097cd204366151b93a52ae786c5296549e58
sugiuta <sugi.uta_1203@icloud.com>
2023-02-22 19:04:51 +0900

    changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-function gate() {
-    var UserInput = prompt("管理者用パスワードを入力して下さい:","");
-    location.href = UserInput + ".html";
+function login() {
+    let userInput = prompt("パスワードを入力して下さい:","");
+    fetch(`https://script.google.com/macros/s/AKfycbyyyTPAO3dET-dRGd3yrSD7_cz-APrMZLp0mu6BATjqxAV9AUfDxNwaicCpIec5w4Aw/exec?password=${userInput}`, {
+        method: "GET"
+    })
+    .then(response => {
+        if (response.ok) return response.json();
+        throw new Error('Network response was not ok.');
+    })
+    .then(result => {
+        let link = JSON.stringify(result.link)
+        link = link.slice(1);
+        link = link.slice(0, -1);
+        location.href = link;
+    })
+    .catch(error => {
+        alert(error);
+    })
 }
